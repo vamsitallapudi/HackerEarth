bf9a5f25406c3b94f5d3e737816250abe2f1916a
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-01 18:35:37 +0530

    Basic Programming - Input/Output - Prime Number

---
+++ src/main/PrimeNumber.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class PrimeNumber {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        ArrayList<Integer> primeNos = new ArrayList<>();
+        for(int i=2;i<n;i++){
+            if(isPrime(i)){
+                primeNos.add(i);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for(int i: primeNos){
+            sb.append(i);
+            sb.append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+
+
+    }
+
+    private static boolean isPrime(int n) {
+
+        for(int i=2;i<=n/2; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
