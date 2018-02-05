d79113e02859911a7fb2426ec0d46b7324275289
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-05 15:34:31 +0530

    Basic Programming - Input/Output - Count Divisors

---
+++ src/main/CountDivisors.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CountDivisors {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        ArrayList<Integer> numList = new ArrayList<>();
+        String[] numListArray = num.split(" ");
+        int k = 0;
+
+        for(String i : numListArray){
+           numList.add(Integer.parseInt(i));
+        }
+
+        for(int i = numList.get(0);i<= numList.get(1); i++){
+            if(i%numList.get(2)==0){
+                k+=1;
+            }
+        }
+        System.out.println(k);
+
+    }
+}
