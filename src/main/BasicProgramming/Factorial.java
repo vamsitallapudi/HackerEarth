e601f767dc64058a161253fa4337d46d5c71ec78
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-06 11:00:03 +0530

    Basic Programming - Input/Output - Factorial

---
+++ src/main/BasicProgramming/Factorial.java
@@ -0,0 +1,19 @@
+package main.BasicProgramming;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(fact(sc.nextInt()));
+    }
+    private static int fact(int n){
+        if(n<=1){
+            return 1;
+        }
+        else {
+            return n * fact(n-1);
+        }
+    }
+}
