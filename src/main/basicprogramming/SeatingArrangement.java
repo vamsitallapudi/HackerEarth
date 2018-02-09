1f155f1d03fc329e3f7ee4d5a9a3bfb95b934f4a
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-09 09:09:22 +0530

    Basic Programming - Input/Output - Seating Arrangement another attempt

--- src/main/basicprogramming/SeatingArrangement.java
+++ src/main/basicprogramming/SeatingArrangement.java
@@ -1,68 +1,60 @@
 package main.basicprogramming;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class SeatingArrangement {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int iterations = sc.nextInt();
-        while (iterations-- > 0) {
-            boolean flag = false;
-            ArrayList<Integer> a = new ArrayList<>(55);
-            ArrayList<Integer> b = new ArrayList<>(55);
+        int iter = sc.nextInt();
 
-            ArrayList<Integer> arrayList = new ArrayList<>(6);
-            for (int i = 0; i < 108; i++) {
-                if ((i) % 6 == 0) {
-                    flag = !flag;
-                    fetchArrayListAndReverseAndClear(b, arrayList);
-                }
-                if (flag) {
-                    a.add(i + 1);
-                } else {
-                    arrayList.add(i + 1);
-                }
-            }
-            if (arrayList.size() > 0) {
-                fetchArrayListAndReverseAndClear(b, arrayList);
-            }
-
+        while (iter-- > 0) {
             int input = sc.nextInt();
-            String str = !flag?fetchSeatType(input%6):fetchSeatType(6-(input%6));
-            if (a.contains(input)) {
-                System.out.println(b.get(a.indexOf(input))+ " " + str);
-            } else if (b.contains(input)) {
-                System.out.println(a.get(b.indexOf(input))+ " " + str);
+            int rem = input %12;
+            int dividend = input/12;
+            HashMap<Integer, String> output = fetchOutput(rem);
+            for ( Map.Entry<Integer, String> entry : output.entrySet()) {
+                Integer key = (dividend-1)* 12 * entry.getKey()+1;
+                String value = entry.getValue();
+                System.out.println(key + " "+ value );
             }
         }
     }
 
-    private static String fetchSeatType(int i) {
-        switch (i){
-            case 0:
-                return "WS";
+    private static HashMap<Integer,String> fetchOutput(int input) {
+        HashMap<Integer,String> hashMap = new HashMap<>();
+        switch (input){
             case 1:
-                return "WS";
-            case 2:
-                return "MS";
-            case 3:
-                return "AS";
-            case 4:
-                return "AS";
-            case 5:
-                return "MS";
-            default:
-                return "";
-
+                hashMap.put(12,"WS");
+                return hashMap;
+            case 0:
+                hashMap.put(1,"WS");
+                return hashMap;
+//            case 6:
+//                return "7 WS";
+//            case 7:
+//                return "6 WS";
+//            case 2:
+//                return "11 MS";
+//            case 11:
+//                return "2 MS";
+//            case 5:
+//                return "8 MS";
+//            case 8:
+//                return "5 MS";
+//            case 3:
+//                return "10 AS";
+//            case 10:
+//                return "3 AS";
+//            case 4:
+//                return "9 AS";
+//            case 9:
+//                return "4 AS";
+                default:
+                    return null;
         }
-
-    }
-
-    private static void fetchArrayListAndReverseAndClear(ArrayList<Integer> d, ArrayList<Integer> arrayList) {
-        Collections.reverse(arrayList);
-        d.addAll(arrayList);
-        arrayList.clear();
     }
 }
