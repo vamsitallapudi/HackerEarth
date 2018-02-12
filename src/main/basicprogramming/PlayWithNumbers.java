ecfb2af541b503e0093585543b5432f831445ffc
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-12 15:34:29 +0530

    Basic Programming - Input/Output - Play with Numbers Time Limit Exceeded soln #2 using fast i/o but still time limit exceeding.

--- src/main/basicprogramming/PlayWithNumbers.java
+++ src/main/basicprogramming/PlayWithNumbers.java
@@ -1,32 +1,148 @@
 package main.basicprogramming;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+
 
 public class PlayWithNumbers {
-    public static void main(String[] args) throws Exception{
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
 
-        long[] array = new long[Integer.parseInt(st.nextToken())];
-        long iter = Long.parseLong(st.nextToken());
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
 
-        StringTokenizer stArray = new StringTokenizer(br.readLine());
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Reader sc = new Reader();
+        int[] array = new int[sc.nextInt()];
+        int iter = sc.nextInt();
         for(int i=0;i<array.length;i++){
-            array[i] = Long.parseLong(stArray.nextToken());
+            array[i] = sc.nextInt();
         }
         while(iter-- > 0){
-            StringTokenizer stInput = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(stInput.nextToken());
-            int b = Integer.parseInt(stInput.nextToken());
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-            long[] temp = Arrays.copyOfRange(array, a-1,b);
+            int[] temp = Arrays.copyOfRange(array, a-1,b);
 
-            long outputFloored = 0L;
-            for(long i: temp){
+            int outputFloored = 0;
+            for(int i: temp){
                 outputFloored +=Math.floor(i);
             }
             System.out.println(outputFloored/temp.length);
