5003b25ff3c522e0ea01ee22c9332bc103d0ea13
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-14 00:21:20 +0530

    Basic Programming - Input/Output - Cipher

---
+++ src/main/basicprogramming/Cipher.java
@@ -0,0 +1,30 @@
+package main.basicprogramming;
+
+import java.util.Scanner;
+
+public class Cipher {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] chars = sc.nextLine().toCharArray();
+        int iter = sc.nextInt();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetterOrDigit(chars[i])) {
+                int value = (int) chars[i];
+                for (int j = 0; j < iter; j++) {
+                    value +=1;
+                    if (value == 91) {
+                        value = 65;
+                    } else if (value == 123) {
+                        value = 97;
+                    } else if (value == 58) {
+                        value = 48;
+                    }
+                }
+                chars[i] = (char) value;
+            }
+        }
+        System.out.println(String.copyValueOf(chars));
+    }
+
+//        Epp-gsrzsCw-3-fi:Epivx5
+}
