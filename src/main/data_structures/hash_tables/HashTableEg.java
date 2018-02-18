5d0b8904937e1299af8259d3247a1bba8e9e254e
Vamsi Tallapudi <vamsi@coderefer.com>
2018-02-18 23:05:03 +0530

    Data Structures - Hash Tables - HashTable Eg #1 - Counting frequency of characters in a string

---
+++ src/main/data_structures/hash_tables/HashTableEg.java
@@ -0,0 +1,22 @@
+package main.data_structures.hash_tables;
+
+public class HashTableEg {
+    public static void main(String[] args) {
+        String s =  "ababcd";
+        char[] chars = s.toCharArray();
+        countFrequency(chars);
+    }
+
+    private static void countFrequency(char[] chars) {
+        for(char i='a';i<='z';i++){
+//            System.out.println(i);
+            int freq = 0;
+            for(int j= 0; j<chars.length;j++){
+                if(chars[j] == i) {
+                    ++freq;
+                }
+            }
+            System.out.println(freq);
+        }
+    }
+}
