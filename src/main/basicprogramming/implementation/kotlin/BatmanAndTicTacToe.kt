516022fd019ae90f5ccffbceaa33b4f850d7d631
Vamsi Tallapudi <vamsi@coderefer.com>
2018-08-02 19:07:59 +0530

    Basic Programming - Implementation - BatmanAndTicTacToe Accepted Answer

--- src/main/basicprogramming/implementation/kotlin/BatmanAndTicTacToe.kt
+++ src/main/basicprogramming/implementation/kotlin/BatmanAndTicTacToe.kt
@@ -42,13 +42,26 @@ fun performDiagonalCheck(line1: String, line2: String, line3: String, line4: Str
     if(diagonalString1.contains("xx.") || diagonalString1.contains(".xx") || diagonalString1.contains("x.x")) return true
     if(diagonalString2.contains("xx.") || diagonalString2.contains(".xx") || diagonalString2.contains("x.x")) return true
 
+//    small diagonals with 3 ends
+    val diagonalString3 = (line1[1].toString() + line2[2] + line3[3])
+    val diagonalString4 = (line1[2].toString() + line2[1] + line3[0])
+    val diagonalString5 = (line2[0].toString() + line3[1] + line4[2])
+    val diagonalString6 = (line2[2].toString() + line3[1] + line4[0])
+    val diagonalString7 = (line2[3].toString() + line3[2] + line4[1])
+
+    if(diagonalString3.contains("xx.") || diagonalString3.contains(".xx") || diagonalString3.contains("x.x")) return true
+    if(diagonalString4.contains("xx.") || diagonalString4.contains(".xx") || diagonalString4.contains("x.x")) return true
+    if(diagonalString5.contains("xx.") || diagonalString5.contains(".xx") || diagonalString5.contains("x.x")) return true
+    if(diagonalString6.contains("xx.") || diagonalString6.contains(".xx") || diagonalString6.contains("x.x")) return true
+    if(diagonalString7.contains("xx.") || diagonalString7.contains(".xx") || diagonalString7.contains("x.x")) return true
+
     return false
 }
 
 fun performColumnCheck(line1: String, line2: String, line3: String, line4: String): Boolean {
     for(i in 0..3) {
         val columnString = (line1[i].toString()+line2[i]+line3[i]+line4[i])
-        if(columnString.contains("xx.") ||columnString.contains("x.x") || columnString.contains("x.x")) {
+        if(columnString.contains("xx.") ||columnString.contains("x.x") || columnString.contains(".xx")) {
             return true
         }
     }
