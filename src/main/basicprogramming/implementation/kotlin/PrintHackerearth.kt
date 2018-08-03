e4c3383a1413c031926f4913610475e97f1b0d53
Vamsi Tallapudi <vamsi@coderefer.com>
2018-08-03 22:39:08 +0530

    Basic Programming - Implementation - PrintHackerearth Accepted answer

--- src/main/basicprogramming/implementation/kotlin/PrintHackerearth.kt
+++ src/main/basicprogramming/implementation/kotlin/PrintHackerearth.kt
@@ -2,6 +2,7 @@ package main.basicprogramming.implementation.kotlin
 
 fun main(args: Array<String>) {
 
+    val length = readLine()!!.toInt()
     val input = readLine()!!
     println(compareWithHEString(input).toString())
 }
@@ -10,16 +11,16 @@ fun compareWithHEString(str:String) :Int{
 
 //  hackerearth characters - 2 a's, 1 c, 2 e's, 2 h's, 1 k, 2 r's, 1 t
 
-    val countOfA = str.count{it == 'a'}
-    val countOfC = str.count{it == 'c'}
-    val countOfE = str.count{it == 'e'}
-    val countOfH = str.count{it == 'h'}
-    val countOfK = str.count{it == 'k'}
-    val countOfR = str.count{it == 'r'}
-    val countOfT = str.count{it == 't'}
+    val countOfA:Int = str.count{it == 'a'}
+    val countOfC:Int = str.count{it == 'c'}
+    val countOfE:Int = str.count{it == 'e'}
+    val countOfH:Int = str.count{it == 'h'}
+    val countOfK:Int = str.count{it == 'k'}
+    val countOfR:Int = str.count{it == 'r'}
+    val countOfT:Int = str.count{it == 't'}
 
-    val ace = minOf(countOfA%2,countOfC, countOfE%2)
-    val hkr = minOf(countOfH%2, countOfK%2, countOfR%2)
+    val ace = minOf(countOfA/2,countOfC, countOfE/2)
+    val hkr = minOf(countOfH/2, countOfK, countOfR/2)
 
     return minOf(ace, hkr, countOfT)
 }
