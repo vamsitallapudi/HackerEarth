3316637fadf36cb5904d45797862041f685f27f2
Sushma Baliza <sushma.baliza23@gmail.com>
2018-08-05 21:11:51 +0530

    Sushma Commit from my branch

--- src/main/basicprogramming/PrimeNumber.java
+++ src/main/basicprogramming/PrimeNumber.java
@@ -28,7 +28,7 @@ public class PrimeNumber {
     }
 
     private static boolean isPrime(int n) {
-
+// To check if a number is prime
         for(int i=2;i<=n/2; i++){
             if(n%i==0){
                 return false;
