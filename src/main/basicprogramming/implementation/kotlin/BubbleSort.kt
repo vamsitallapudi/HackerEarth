6628527ee0d0e3e741290671935ea3d8fb4dec6d
Vamsi Tallapudi <vamsi@coderefer.com>
2018-10-11 19:00:29 +0530

    Bubble Sort

---
+++ src/main/basicprogramming/implementation/kotlin/BubbleSort.kt
@@ -0,0 +1,33 @@
+package main.basicprogramming.implementation.kotlin
+
+
+fun main(args: Array<String>) {
+    var numSwaps = 0
+    var count = readLine()!!.toInt()
+    var isSorted = false
+    val str = readLine()!!
+    val intList: ArrayList<Int> = ArrayList(str.split(" ").map { it.toInt() })
+
+    var lastUnsorted = intList.size - 1
+
+    while (!isSorted) {
+        isSorted = true
+        for (i in 0 until lastUnsorted) {
+            if (intList[i] > intList[i + 1]) {
+                swap(intList, i, i + 1)
+                numSwaps++
+                isSorted = false
+            }
+        }
+        lastUnsorted--
+    }
+    println("Array is sorted in $numSwaps swaps.")
+    println("First Element: ${intList[0]}")
+    println("Last Element: ${intList[intList.size - 1]}")
+}
+
+fun swap(list: ArrayList<Int>, a: Int, b: Int) {
+    val temp = list[b]
+    list[b] = list[a]
+    list[a] = temp
+}
