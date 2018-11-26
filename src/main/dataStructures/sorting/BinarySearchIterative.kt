d645dea6b960145c7ea8ff28b363b4630689f458
Vamsi Tallapudi <vamsi@coderefer.com>
2018-11-26 18:36:27 +0530

    Binary Search Iterative

---
+++ src/main/dataStructures/sorting/BinarySearchIterative.kt
@@ -0,0 +1,27 @@
+package main.dataStructures.sorting
+
+fun main(args: Array<String>) {
+    val input = readLine()!!.trim().split(" ").map { it -> it.toInt() }.toIntArray()
+    val eleToSearch = readLine()!!.trim().toInt()
+    val pos = binarySearchIterative(input, eleToSearch)
+    if(pos >= 0 ) {
+        println(pos)
+    } else {
+        println("Position not found")
+    }
+}
+
+fun binarySearchIterative(input: IntArray, eleToSearch: Int) : Int{
+    var low = 0
+    var high = input.size-1
+    var mid:Int
+    while(low <= high) {
+        mid = (low + high)/2
+        when {
+            eleToSearch >input[mid]   -> low = mid+1
+            eleToSearch == input[mid] -> return mid
+            eleToSearch < input[mid]  -> high = mid-1
+        }
+    }
+    return -1
+}
