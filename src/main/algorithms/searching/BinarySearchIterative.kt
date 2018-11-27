65c818ac8b46b61f9b92a31b1c8565ef27edbdca
Vamsi Tallapudi <vamsi@coderefer.com>
2018-11-27 08:24:34 +0530

    Comments to compliment the blog post on Binary Search

--- src/main/algorithms/searching/BinarySearchIterative.kt
+++ src/main/algorithms/searching/BinarySearchIterative.kt
@@ -1,11 +1,11 @@
 package main.algorithms.searching
 
 fun main(args: Array<String>) {
-    val input = readLine()!!.trim().split(" ").map { it -> it.toInt() }.toIntArray()
-    val eleToSearch = readLine()!!.trim().toInt()
+    val input = readLine()!!.trim().split(" ").map { it -> it.toInt() }.toIntArray()  // to read an array (from user input)
+    val eleToSearch = readLine()!!.trim().toInt()   // to read the element to be searched (from user input)
     val pos = binarySearchIterative(input, eleToSearch)
     if(pos >= 0 ) {
-        println(pos)
+        println(pos)    // to print position at last
     } else {
         println("Position not found")
     }
@@ -18,9 +18,9 @@ fun binarySearchIterative(input: IntArray, eleToSearch: Int) : Int{
     while(low <= high) {
         mid = (low + high)/2
         when {
-            eleToSearch >input[mid]   -> low = mid+1
-            eleToSearch == input[mid] -> return mid
-            eleToSearch < input[mid]  -> high = mid-1
+            eleToSearch >input[mid]   -> low = mid+1    // element is greater than middle element of array, so it will be in right half of array
+            eleToSearch == input[mid] -> return mid // found the element
+            eleToSearch < input[mid]  -> high = mid-1   //element is less than middle element of array, so it will be in left half of the array.
         }
     }
     return -1
